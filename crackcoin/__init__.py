f3cecab857c9eacb70f592fd332b2803abef8da3
tomssteller2020 <77651011+tomssteller2020@users.noreply.github.com>
2021-01-18 20:25:58 -0600

    Update __init__.py

--- crackcoin/__init__.py
+++ crackcoin/__init__.py
@@ -1,7 +1,4 @@
-import databasing.py, mining.py, networks.py, eccs.py, encodings.py, networks.py, threads.py
-from hashlib import sha256
-
-db 				= databasing.crackDB()
+db 			= databasing.crackDB()
 miner			= mining.crackcoinMiner()
 network 		= networks.crackcoinNetwork()
 
